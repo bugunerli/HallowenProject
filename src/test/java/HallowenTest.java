eb01f814c0e3c4d7b0b8b20a29a6a774335d58fb
bugunerli <gunerlibulent@gmail.com>
2020-10-31 10:42:56 -0600

    no treat or trick commit

--- src/test/java/HallowenTest.java
+++ src/test/java/HallowenTest.java
@@ -1,5 +1,10 @@
 public class HallowenTest {
 
     // I am about to give everyone candy for hallowen
+    // are we there yet???? Hello Hello
+    // lets play a game
+    // its fun to drive so far
 
+    // there will be no treat and trick this year
+    // stay at home and be safe and wear your mask outside
 }
